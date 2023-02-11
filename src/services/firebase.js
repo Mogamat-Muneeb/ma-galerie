ea36b148f628d3db3e8a00167991f21c5e441f29
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-02-11 09:09:15 +0200

    wip

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: "AIzaSyBF414Xe0rgT4Auk6bFPtNrg1ABtlARQYw",
-  authDomain: "ma-galery.firebaseapp.com",
-  projectId: "ma-galery",
-  storageBucket: "ma-galery.appspot.com",
-  messagingSenderId: "8866788089",
-  appId: "1:8866788089:web:0c74657fc6739b9d2be828",
-  measurementId: "G-945F35FS3Q"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MESSUREMENT_ID
 };
 
 
