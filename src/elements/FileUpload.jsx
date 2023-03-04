2f103f0106c9f1fd24d5894e1eadec1531589164
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-03-04 13:13:36 +0200

    wip

--- src/elements/FileUpload.jsx
+++ src/elements/FileUpload.jsx
@@ -6,8 +6,7 @@ import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 
 const FileUpload = (props) => {
-
-    // console.log("currentUser FileUpload", props.currentUser)
+  // console.log("currentUser FileUpload", props.currentUser)
   const [file, setFile] = useState("");
   const [uploadProgress, setUploadProgress] = useState(0);
   const [images, setImages] = useState([]);
@@ -17,8 +16,8 @@ const FileUpload = (props) => {
   const [open, setOpen] = useState(false);
   const closeModal = () => setOpen(false);
   const openModal = () => setOpen(true);
- const userUsing = props.currentUser
- console.log("userUsing", userUsing)
+  const userUsing = props.currentUser;
+  console.log("userUsing", userUsing);
 
   useEffect(() => {
     loadAllImages();
@@ -32,15 +31,16 @@ const FileUpload = (props) => {
 
   const loadAllImages = async () => {
     setLoading(true);
-    const querySnapshot = await getDocs(collection(db, "notes", userUsing, "user"));
-        const images = querySnapshot.docs.map((doc) => ({
+    const querySnapshot = await getDocs(
+      collection(db, "notes", userUsing, "user")
+    );
+    const images = querySnapshot.docs.map((doc) => ({
       ...doc.data(),
       id: doc.id,
     }));
     setImages(images);
     setLoading(false);
   };
-
 
   useEffect(() => {
     loadAllImages();
@@ -53,6 +53,7 @@ const FileUpload = (props) => {
   const handleUpload = () => {
     if (file == "") {
       alert("Please add the file");
+      return;
     }
 
     const storageRef = ref(storage, `images/${file.name}`);
@@ -61,6 +62,26 @@ const FileUpload = (props) => {
 
     setUploaded(false);
 
+    // uploadTask.on(
+    //   "state_changed",
+    //   (snapshot) => {
+    //     const progress =
+    //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    //     setUploadProgress(progress);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   },
+    //   () => {
+    //     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+    //       const imageStoreRef = doc(db, "notes", userUsing, "user", file.name);
+    //       setDoc(imageStoreRef, {
+    //         imageUrl: downloadURL,
+    //       });
+    //     });
+    //     setUploaded(true);
+    //   }
+    // );
     uploadTask.on(
       "state_changed",
       (snapshot) => {
@@ -76,39 +97,53 @@ const FileUpload = (props) => {
           const imageStoreRef = doc(db, "notes", userUsing, "user", file.name);
           setDoc(imageStoreRef, {
             imageUrl: downloadURL,
+          }).then(() => {
+            setUploaded(true);
+            closeModal();
+            loadAllImages();
           });
         });
-        setUploaded(true);
       }
     );
   };
 
   return (
     <>
-    <div className="flex justify-center ">
-    {loading && <p>Loading galleryvv...</p>}
-    </div>
-    <div className="flex items-center justify-center">
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
-      {images &&
-        images.map((imageUrl, id) => {
-          return (
-            <div className="flex w-full gap-3" key={id}>
-              <img src={imageUrl.imageUrl} className="w-[150px] h-[150px] object-cover"/>
-            </div>
-          );
-        })}
-        <button onClick={openModal} className="w-[150px] h-[150px] bg-[#1D1D1D] text-white flex items-center justify-center text-center">Upload an Image</button>
+      <div className="flex justify-center ">
+        {loading && (
+          <div class=" flex justify-center items-center">
+            <div class="animate-spin rounded-full h-5 w-5 border-b-2 border-black"></div>
+          </div>
+        )}
       </div>
-      <Popup open={open} onClose={closeModal}>
-        <input type="file" accept="/image/*" onChange={handleChange}></input>
-        <button onClick={handleUpload}>Save</button>
-        {uploaded && (
-          <p className="success-msg">Image was uploaded successfully</p>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-3">
+          {images &&
+            images.map((imageUrl, id) => {
+              return (
+                <div className="flex w-full gap-3 shadow-xl" key={id}>
+                  <img
+                    src={imageUrl.imageUrl}
+                    className="w-[150px] h-[150px] object-cover"
+                  />
+                </div>
+              );
+            })}
+          <button
+            onClick={openModal}
+            className="w-[150px] h-[150px] bg-[#1D1D1D] text-white flex items-center justify-center text-center"
+          >
+            Upload an Image
+          </button>
+        </div>
+        <Popup open={open} onClose={closeModal}>
+          <input type="file" accept="/image/*" onChange={handleChange}></input>
+          <button onClick={handleUpload}>Save</button>
+          {uploaded && (
+            <p className="success-msg">Image was uploaded successfully</p>
           )}
-      </Popup>
-    </div>
-    
+        </Popup>
+      </div>
     </>
   );
 };
